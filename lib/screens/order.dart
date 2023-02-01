0130396357e39e750a6a4b9c645f2d4dfe8c07af
KhadijahArizal <116158646+KhadijahArizal@users.noreply.github.com>
2023-02-01 22:24:07 +0800

    Create order.dart

---
+++ lib/screens/order.dart
@@ -0,0 +1,19 @@
+class Order {
+  dynamic name;
+  dynamic phoneNo;
+  dynamic size;
+  dynamic quantity;
+  dynamic color;
+  dynamic time;
+  dynamic deliver;
+
+
+  Order(
+      {required this.name,
+      required this.phoneNo,
+      required this.size,
+      required this.quantity,
+      required this.color,
+      required this.time,
+      required this.deliver});
+}
