46dab38b83e37f7b90503d82b64a95ad54e98311
KhadijahArizal <116158646+KhadijahArizal@users.noreply.github.com>
2023-02-01 22:23:30 +0800

    Update and rename contactUs.dart to contactUs_screen.dart

--- lib/screens/contactUs_screen.dart
+++ lib/screens/contactUs_screen.dart
@@ -1,5 +1,5 @@
-import 'package:details/main.dart';
-import 'package:details/routes.dart';
+import 'package:dino_printing/main.dart';
+import 'package:dino_printing/router/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:url_launcher/url_launcher.dart';
