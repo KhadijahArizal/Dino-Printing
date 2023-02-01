1a4f51c6a8354a48cd60d82bd115c13a902e1384
KhadijahArizal <116158646+KhadijahArizal@users.noreply.github.com>
2023-02-01 22:21:50 +0800

    Update routes.dart

--- lib/router/routes.dart
+++ lib/router/routes.dart
@@ -1,8 +1,11 @@
 
+
 class Routes {
-  static const orderForm = '/orderForm';
+  static const orderForm = '/orderform_screen';
   static const userData = '/user_orderD';
-  static const profile = '/profile';
-  static const location = '/loc_screen';
+  static const profile = '/profile_screen';
+  static const location = '/location_screen';
+  static const contactUs = '/contactUs_screen';
+  static const orderDetail = '/orderdetail';
   static const homeScreen = '/home_screen';
 }
