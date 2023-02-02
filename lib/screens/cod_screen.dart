bb1c050df44ad690b0698a8a6c34c0400316c581
Zahra Fathanah <97120878+kangmanis@users.noreply.github.com>
2023-02-03 01:38:06 +0800

    Update cod_screen.dart

--- lib/screens/cod_screen.dart
+++ lib/screens/cod_screen.dart
@@ -10,37 +10,52 @@ class CodOption extends StatefulWidget {
 }
 
 class _CodOptionState extends State<CodOption> {
- 
- @override
+  @override
   Widget build(BuildContext context) {
-    final ButtonStyle style =
-        ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
-
-    
-      AppBar(
-        title: const Text('Payment via COD (Cash on Delivery'),
-      );
-    return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-         Container(
-          alignment: Alignment.center,
-          child: Image.asset('assets/card.png'),
-          ),
-          ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.indigoAccent,
-          ),
-          onPressed: () {
-             Navigator.pushNamed(context, Routes.homeScreen);
-          },
-          child: const Text('Back to Home'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Payment via Cash On Delivery'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Container(
+              height: 350,
+              alignment: Alignment.center,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Image.asset('assets/card.png',
+                    width: 300, height: 300, fit: BoxFit.cover),
+              ),
+            ),
+            const Text('Thank You',
+                style: TextStyle(
+                    fontSize: 17,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w600)),
+            const Text('Your order is in proccess',
+                style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.black54,
+                    fontWeight: FontWeight.w500,
+                    textBaseline: TextBaseline.alphabetic)),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.indigoAccent,
+              ),
+              onPressed: () {
+                Navigator.pushNamed(context, Routes.homeScreen);
+              },
+              child: const Text('My Order'),
+            ),
+          ],
         ),
-        
-        ],
       ),
     );
- 
   }
 }
+
